38102e51cb1d418a384f5a7244cb04df2c6c1a48
Dzhambulat <dzhambulat.kagermanov@gmail.com>
2024-12-28 20:58:11 +0300

    UPDATE: Created Sort filter modal content in home page

--- src/components/widgets/HomeSortModal/index.tsx
+++ src/components/widgets/HomeSortModal/index.tsx
@@ -1,15 +1,44 @@
-import { FC } from 'react'
+'use client'
+import { FC, useState } from 'react'
 import { TClassName } from '@/types'
-import { ModalBase } from '@/components/ui'
+import { ModalBase, Typography } from '@/components/ui'
 import { HOME_SORT_MODAL } from '@/constants'
 import { cn } from '@/lib'
+import { Item } from './Item'
+import { slugs } from './constants/slugs'
 import cls from './index.module.scss'
 
 interface Props extends TClassName {}
 const HomeSortModal: FC<Props> = ({ className }) => {
+	const [sortItem, setSortItem] = useState<string>('По дате публикации')
+
 	return (
 		<ModalBase slug={HOME_SORT_MODAL} className={cn(cls.wrapper, [className])}>
-			3
+			<div className={cn(cls.content)}>
+				<Typography
+					tag='h2'
+					font='Inter-SB'
+					size={18}
+					className={cn(cls.title)}
+				>
+					Сортировка:
+				</Typography>
+				<ul className={cn(cls.group)}>
+					{slugs.map(slug => {
+						return (
+							<Item
+								activeSlug={sortItem}
+								setActiveSlug={setSortItem}
+								key={slug}
+								slug={slug}
+								tag='li'
+								className={cn(cls.item)}
+								onClick={() => {}}
+							/>
+						)
+					})}
+				</ul>
+			</div>
 		</ModalBase>
 	)
 }
