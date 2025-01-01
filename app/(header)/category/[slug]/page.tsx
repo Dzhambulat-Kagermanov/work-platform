3e5e5b258071eed1448b581b9aa626f07f96ee8e
Dzhambulat <dzhambulat.kagermanov@gmail.com>
2025-01-01 10:10:52 +0300

    FIX: Fixed deploy type error

--- app/(header)/category/[slug]/page.tsx
+++ app/(header)/category/[slug]/page.tsx
@@ -1,9 +1,16 @@
 import { CategoryItemPage } from '@/components/page/CategoryItem'
 import { FC } from 'react'
 
-interface Props {}
-const CategoryItem: FC<Props> = ({}) => {
-	return <CategoryItemPage />
+interface Props {
+	searchParams: Promise<{
+		category: string
+		subcategory: string
+	}>
+}
+const CategoryItem: FC<Props> = async ({ searchParams }) => {
+	const { category, subcategory } = await searchParams
+
+	return <CategoryItemPage slug={category} subcategory={subcategory} />
 }
 
 export default CategoryItem
