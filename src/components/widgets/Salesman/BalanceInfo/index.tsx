6af5814317198cc652d7117992df58d04e5b3828
Dzhambulat <dzhambulat.kagermanov@gmail.com>
2025-04-05 13:43:12 +0300

    FIX: Disable pages router

--- src/components/widgets/Salesman/BalanceInfo/index.tsx
+++ src/components/widgets/Salesman/BalanceInfo/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC, useState } from "react";
+import { FC } from "react";
 import { TClassName } from "@/types";
 import { cn } from "@/lib";
 import { Content } from "./Content";
